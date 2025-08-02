d1463561f01c9e1e11f4be6b1f46d86a904733a0
anwann16 <andiwann1603@gmail.com>
2025-08-02 14:13:36 +0700

    Fix error deploy on vercel

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,10 +2,12 @@ import { Project } from "@/types/component";
 import ProjectCard from "@/components/ProjectCard";
 import SectionTitle from "@/components/SectionTitle";
 import PageContainer from "@/components/PageContainer";
+import { getProjects } from "@/lib/getProjects";
 
 const ProjectPage = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projects`);
-  const projects = await res.json();
+  const projects = await getProjects();
+
+  console.log(projects);
 
   return (
     <PageContainer>
@@ -13,7 +15,7 @@ const ProjectPage = async () => {
       <div className="min-h-screen my-10">
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 gap-8">
-            {projects.data.map((project: Project, index: number) => (
+            {projects.map((project: Project, index: number) => (
               <ProjectCard key={`project-${index}`} project={project} />
             ))}
           </div>
---
+++ lib/getProjects.ts
@@ -0,0 +1,10 @@
+import { Project } from "@/types/component";
+
+export const getProjects = async (): Promise<Project[]> => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/projects`);
+  const response = await res.json();
+  const projects: Project[] = await response.data;
+
+  return projects;
+};
